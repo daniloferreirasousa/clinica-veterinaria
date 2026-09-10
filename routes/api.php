e7ff5f89ec23b0fb9b27f57e0e44d0125641972b
Danilo De Andrade Ferreira Sousa <ddsousa@firjan.com.br>
2026-09-10 19:52:10 -0300

    fix: corrige quebra de icones com o Paginator no AppServiceProvider.php

--- routes/api.php
+++ routes/api.php
@@ -2,16 +2,17 @@
 
 use App\Http\Controllers\Api\AnimalController;
 use App\Http\Controllers\Api\AuthController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // Endpoit aberto: Login e Emissão de Tokens
 Route::post('/login', [AuthController::class, 'login']);
 
 // Endpoints Protegidos por Bearer Token via Sanctum
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware('auth:sanctum')->name('api')->group(function () {
 
     Route::post('/logout', [AuthController::class, 'logout']);
 
-    Route::apiResource('animals', AnimalController::class);
+    Route::apiResource('animals', AnimalController::class)->parameters([
+        'animals' => 'animal',
+    ]);
 });
